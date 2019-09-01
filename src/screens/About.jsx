6fe9fb1b2ab6aab47cf48d86cb042e28bb7136a0
Adarsh Nanwani <adarsh.nanwani@pratian.com>
2019-09-01 18:22:38 +0530

    Add screen component skeletons

---
+++ src/screens/About.jsx
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import {Text} from 'react-native';
+
+class About extends Component{
+  render(){
+    return <Text>This is the About page</Text>;
+  }
+}
+
+export default About;
---
+++ src/screens/Game.jsx
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+class Game extends Component {
+  render() {
+    return <Text>This is the Game</Text>;
+  }
+}
+
+export default Game;
